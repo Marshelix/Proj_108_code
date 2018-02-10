865a48a9e72793236348adf6aac8157d7f6dce43
marshelix <martin.sanner@gmx.de>
2018-02-10 18:16:54 +0000

    spherharg working correctly, can now generate spherical harmonics.
    Signed-off-by: marshelix <martin.sanner@gmx.de>

--- Code/Python/spherical_harmonics_generator/spherharg.py
+++ Code/Python/spherical_harmonics_generator/spherharg.py
@@ -17,11 +17,14 @@ import pickle
 import scipy.special as f_base
 import math
 import numpy as np
-import os                              #filesaving
+import time
+
+start_time = time.time()
+
 l0 = int(input("l0 = "))
 l1 = int(input("l1 = "))
 
-ang_res = 0.01
+ang_res = 0.01  #resolution of the angle. Number of points N = 2pi/ang_res for both cases
 
 def calculate_harmonics(l0,l1):
     '''
@@ -33,23 +36,21 @@ def calculate_harmonics(l0,l1):
         l0 = int(input("l0 = "))
         l1 = int(input("l1 = "))
         return calculate_harmonics(l0,l1)
-    print("Generating vectors")
+   
     N = int(2*math.pi/ang_res)
     thetas = np.linspace(0,2*math.pi,N)
     phis = np.linspace(0,math.pi,N)
-    print("Vectors of size "+str(N) +" created.")
+    
     arrays = []
+    
     for l in range(l0,l1):
         print("===============")
         print("l = " +str(l))
         l_arr = []
         for m in range(-l,l+1):
-            print("+++++++++++++")
-            print("m = "+str(m))
-            print("Creating frame")
+   
             m_frame = pd.DataFrame(0,index = thetas,columns =[str(x) for x in phis] )
-            #m_frame.set_index(thetas)   #set thetas to be the index.
-            print("Frame "+str(m) +" created.")
+            
             '''
             m_frame = 
                   \Phis| ---------------------------
@@ -61,23 +62,26 @@ def calculate_harmonics(l0,l1):
             +
             ...
             '''
-            for theta in thetas:
-                for phi in phis:
-                    func = f_base.sph_harm(l,m,theta,phi)
-                    #bug: For eg m = -1, func is NaN, which m_frame complains about and breaks.
-                    m_frame.set_value(theta,str(phi),func)
+            
+            func = f_base.sph_harm(l,m,thetas,phis)
+            #func is the matrix of values for lm for all thetas, phis
+            m_frame.set_value(thetas,str(phis),func)    #casted into m_frame
             l_arr.append(m_frame)
             
-            print("+++++++++++++")    
+
         arrays.append(l_arr)
         #note: replace path with profile on machine! Needs to be done by user!
         path = "F:\Programming\Project\git\Code\Python\spherical_harmonics_generator\harmonics/"
         file = open(path + "spher_har_l_"+str(l)+".dat","wb")
         pickle.dump(l_arr,file)
         file.close()
+        print("File "+path + "spher_har_l"+str(l)+".dat"+" generated and saved.")
     
     return arrays
 
                     
-array = calculate_harmonics(l0,l1)
-
+calculate_harmonics(l0,l1)
+end_time = time.time()
+elapsed = end_time - start_time
+print("<===========================>")
+print(str(elapsed) + " s = "+str(elapsed/60) +" min = "+str(elapsed/3600) +" h = "+str(elapsed/(3600*24)) +" days passed")
